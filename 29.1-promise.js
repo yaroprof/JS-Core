771f599a8a4e45c9f427b530a3ed845629d37c0e
yaroprof <crosskyiv@gmail.com>
2022-08-11 12:44:30 +0300

    add promise

--- 29.1-promise.js
+++ 29.1-promise.js
@@ -1,1 +1,43 @@
 // --- Promise
+// - Ex01
+// let count = true
+// const countValue = new Promise(function (resolve,reject) {
+//   if(count){
+//     resolve('There is a count value')
+//   }else{
+//     reject('There is no count value')
+//   }
+// })
+// console.log(countValue);
+
+// - Ex02 ---> with 'Then'
+// return a Promise
+// let countValue= new Promise(function(resolve, reject) {
+//   resolve("Promise resolved")
+// })
+// // executes when Promise is resolved succesfully
+// countValue
+// .then(function successValue(result) {
+//   console.log(result);
+// })
+// .then(function successValue1() {
+//   console.log("You can call multiple functions this way.");
+// })
+
+// - Ex03 ---> with 'Catch'
+const countValue = new Promise(function (resolve, reject) {
+  reject('Promise rejected')
+})
+countValue.then(function successValue(result) {
+  console.log(result);
+})
+.catch(function errorValue(result) {
+  console.log(result);
+})
+
+
+
+
+
+
+
