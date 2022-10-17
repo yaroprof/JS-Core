13c02304a73579953bae49ba09140b3d9cd4a27d
yaroprof <crosskyiv@gmail.com>
2022-10-17 13:57:36 +0300

    next commit

--- 02-variables01.js
+++ 02-variables01.js
@@ -4,17 +4,15 @@
 // let myName
 // console.log(myName);  // undefined
 //03
-// let myName
-// console.log(myName);  // undefined
-// myName = 'John'
-// console.log(myName);  // John
+// let myName;
+// console.log(myName); // undefined
+// myName = 'John';
+// console.log(myName); // John
 // 04
 // console.log(console.log(10))
 
 // 05 === const
 // const myName  // Missing initializer in const declaration - не ініціалізована змінна const
-// const myName = 'Peter'
-// myName = 'Bob'
-// console.log(myName);  // Assignment to constant variable.
-
-
+// const myName = 'Peter';
+// myName = 'Bob';
+// console.log(myName); // Assignment to constant variable.
--- 04-dynamicTyping.js
+++ 04-dynamicTyping.js
@@ -14,11 +14,11 @@
 // console.log(foo);  // 10  -> змінили тип змінної з function на number, яке вже - не функція
 // // foo()  // foo is not a function
 
-// -> unmutable 
-const foo = () =>{
+// -> unmutable
+const foo = () => {
   console.log('Howdy'); // Howdy
-}
-foo()
+};
+foo();
 
-foo = 'string'
-console.log(foo);  // Assignment to constant variable.
+foo = 'string';
+console.log(foo); // Assignment to constant variable.
